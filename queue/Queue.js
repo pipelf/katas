3eb69d6ca9a4f09b4ab0c894eee5b4f5850332f8
Luis Felipe Garcia <garcia.luisf@gmail.com>
2017-03-28 15:25:07 +0000

    formating

--- queue/Queue.js
+++ queue/Queue.js
@@ -4,44 +4,41 @@ var QueueNode = require('./Queue');
 function Queue() {
     this.first = null;
     this.last = null;
-    this.size = 0;
 }
 
 Queue.prototype.add = function(data) {
-    //add to the end
     var newnode = new QueueNode(data);
-    
-    if(this.last ===  null) {
-        
+
+    if (this.last === null) {
+
         this.last = newnode;
         this.first = newnode;
-        this.size++;
         return;
     }
-    
+
     this.last.next = newnode;
     this.last = newnode;
 };
 
-Queue.prototype.remove = function () {
-    if(this.isEmpty()) {
+Queue.prototype.remove = function() {
+    if (this.isEmpty()) {
         console.log('queue is empty');
     }
-    
+
     var toremove = this.first;
     this.first = this.first.next;
-    
+
     return toremove;
 };
 
-Queue.prototype.peek = function () {
-     if(this.isEmpty()) {
+Queue.prototype.peek = function() {
+    if (this.isEmpty()) {
         console.log('queue is empty');
     }
-    
+
     return this.first;
 };
 
-Queue.prototype.isEmpty = function () {
-     return this.first === null;
+Queue.prototype.isEmpty = function() {
+    return this.first === null;
 };
