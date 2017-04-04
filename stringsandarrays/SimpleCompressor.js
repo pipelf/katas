021e0ccc527691d151a5ad447536e69a79a89b3b
Luis Felipe Garcia <garcia.luisf@gmail.com>
2017-04-04 03:52:02 +0000

    change string concat to array string builder

--- stringsandarrays/SimpleCompressor.js
+++ stringsandarrays/SimpleCompressor.js
@@ -1,22 +1,23 @@
 'use strict';
 
 class SimpleCompressor {
-    
+
     compress(inputstr) {
-        let output = '';
+        let outarray = [];
         let count = 1;
-        
-        for(let i = 0; i < inputstr.length; i++) {
-            if((inputstr[i] !== inputstr[i + 1]) || inputstr.length === i + 1) {
-                output += inputstr[i] + count;
+
+        for (let i = 0; i < inputstr.length; i++) {
+            if ((inputstr[i] !== inputstr[i + 1]) || inputstr.length === i + 1) {
+                outarray.push(inputstr[i]);
+                outarray.push(count);
                 count = 1;
             }
-            else{
+            else {
                 count++;
             }
         }
-        
-        return output.length > inputstr.length ? inputstr : output;
+
+        return outarray.length > inputstr.length ? inputstr : outarray.join('');
     }
 }
 
