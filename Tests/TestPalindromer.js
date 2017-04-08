8b0430c5935755739a47d9d7fcb1202de88cfd66
Luis Felipe Garcia <garcia.luisf@gmail.com>
2017-04-08 14:04:44 +0000

    Largest palindrome size in string

--- Tests/TestPalindromer.js
+++ Tests/TestPalindromer.js
@@ -7,21 +7,46 @@ var palin = new Palindromer();
 
 testRunner.runTest('TestIsPalindromePermutation_true_for_validstr', () => {
     var str = 'Tact coa';
-    var result = palin.IsPalindromePermutation(str);
+    var result = palin.isPalindromePermutation(str);
 
     assert.equal(result, true, 'is Palindrome permutation')
 });
 
 testRunner.runTest('TestIsPalindromePermutation_false_for_random', () => {
     var str = 'asdf fjfjd k jkjff';
-    var result = palin.IsPalindromePermutation(str);
+    var result = palin.isPalindromePermutation(str);
 
     assert.equal(result, false, 'is Not Palindrome permutation')
 });
 
 testRunner.runTest('TestIsPalindromePermutation_true_for_sentence_noodd', () => {
     var str = 'asddsa';
-    var result = palin.IsPalindromePermutation(str);
+    var result = palin.isPalindromePermutation(str);
 
     assert.equal(result, true, 'is Palindrome permutation')
 });
+
+// "absdfpweraaabcbaa823afhsdft21tacoocat12"
+// output: 12
+// Pali1: aabcbaa
+// Pali2: 21tacoocat12
+testRunner.runTest('Find the lenght Longest Palindrome with no odd center', () => {
+    var input = "absdfpweraaabcbaa823afhsdft21tacoocat12";
+    
+    var result = palin.findlengthoflargestPalindrome(input);
+    
+    assert.equal(result, 12);
+});
+
+testRunner.runTest('Find the lenght Longest Palindrome with odd center', () => {
+    var input = "absdfpweraaabcbaa823afhsdft21tacoxocat12";
+    
+    var result = palin.findlengthoflargestPalindrome(input);
+    
+    assert.equal(result, 13);
+});
+
+
+
+
+
--- stringsandarrays/Palindromer.js
+++ stringsandarrays/Palindromer.js
@@ -1,42 +1,85 @@
-//Given a string check if it is a permutation of a palindrome
+'use strict'
 
-function Palindromer() {};
+class Palindromer {
 
-Palindromer.prototype.IsPalindromePermutation = function (str) {
-    var str = str.replace(' ', '').toLowerCase();
-    var hashstrings = this.createInitArray(128, 0);
+    isPalindromePermutation(str) {
+        var str = str.replace(' ', '').toLowerCase();
+        var hashstrings = this.createInitArray(128, 0);
 
-    //Get count of each string
-    for (var i = 0; i < str.length; i++) {
-        hashstrings[str.charCodeAt(i)]++;
-    }
-
-    var oddcount = 0;
-    //Check if counts are multiples of 2 for all but one
-    for (var i = 0; i < hashstrings.length; i++) {
-        if (oddcount > 1) {
-            return false;
+        //Get count of each string
+        for (var i = 0; i < str.length; i++) {
+            hashstrings[str.charCodeAt(i)]++;
         }
 
-        var istwomult = hashstrings[i] % 2 == 0;
+        var oddcount = 0;
+        //Check if counts are multiples of 2 for all but one
+        for (var i = 0; i < hashstrings.length; i++) {
+            if (oddcount > 1) {
+                return false;
+            }
 
-        if (!istwomult) {
-            oddcount++;
+            var istwomult = hashstrings[i] % 2 == 0;
+
+            if (!istwomult) {
+                oddcount++;
+            }
         }
+
+        return oddcount == 0 || oddcount == 1;
     }
 
-    return oddcount == 0 || oddcount == 1;
-}
+    createInitArray(size, initval) {
+        var array = new Array(size);
 
-Palindromer.prototype.createInitArray = function(size, initval) {
-    var array = new Array(size);
+        for (var j = 0; j < array.length; j++) {
+            array[j] = initval;
+        }
 
-    //initialize array
-    for (var j = 0; j < array.length; j++) {
-        array[j] = initval;
+        return array;
     }
-    
-    return array;
+
+    findlengthoflargestPalindrome(input) {
+        if (input.length === 0) return 0;
+
+        var countoflargest = 0;
+
+        for (var p = 0; p < input.length; p++) {
+            var start,
+                end,
+                odd = false;
+
+            if (input.charAt(p) === input.charAt(p + 1)) {
+                start = p;
+                end = p + 1;
+            }
+            else if ((p - 1) >= 0 && input.charAt(p - 1) === input.charAt(p + 1)) {
+                start = p - 1;
+                end = p + 1;
+                odd = true;
+            }
+            else {
+                continue;
+            }
+
+            var currenthalfcount = 0;
+
+            while (input.charAt(start) === input.charAt(end) && start >= 0 && end < input.length) {
+                start--;
+                end++;
+
+                currenthalfcount++;
+            }
+
+            var totalcurrentcount = odd ? (currenthalfcount * 2) + 1 : (currenthalfcount * 2);
+
+            if (totalcurrentcount > countoflargest) {
+                countoflargest = totalcurrentcount;
+            }
+        }
+
+        return countoflargest;
+    }
+
 }
 
 module.exports = Palindromer;
